510ff2841d0294607a65046c5a0b26d3445a420b
richearda <richearda1996@gmail.com>
2022-04-23 17:52:14 +0800

    Add service model and interface.

---
+++ API/Services/Abstract/IActivityService.cs
@@ -0,0 +1,18 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services.Abstract
+{
+    public interface IActivityService
+    {
+        int AddActivity(Activity activity);
+        int UpdateActivity(Activity activity);
+        int DeleteActivity(int activityId);
+        Task<List<Activity>>  GetCompletedActivities();
+        Task<List<Activity>>  GetPendingActivities();
+        
+    }
+}
---
+++ API/Services/ActivityService.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Entities;
+using API.Services.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class ActivityService : IActivityService
+    {
+        private DataBaseContext _dbContext;
+        public ActivityService(DataBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int AddActivity(Activity activity)
+        {
+             _dbContext.Activities.Add(activity);
+            return _dbContext.SaveChanges();
+            
+        }
+        public int DeleteActivity(int activityId)
+        {
+            Activity deleteActivity = _dbContext.Activities.AsNoTracking().Where(a => a.Id == activityId).FirstOrDefault();
+            _dbContext.Entry(deleteActivity).State = EntityState.Deleted;
+            return _dbContext.SaveChanges();
+        }
+
+        public async Task<List<Activity>> GetCompletedActivities()
+        {
+            return await  _dbContext.Activities.AsNoTracking().Where(a => a.Status == "Completed").ToListAsync();
+      
+        }
+        public async Task<List<Activity>> GetPendingActivities()
+        {
+            return await _dbContext.Activities.AsNoTracking().Where(a => a.Status == "Pending").ToListAsync();
+
+        }     
+        public int UpdateActivity(Activity activity)
+        {
+            _dbContext.Entry(activity).State = EntityState.Modified;
+            return _dbContext.SaveChanges();
+        }
+    }
+}
